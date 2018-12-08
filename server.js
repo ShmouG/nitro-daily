88580d46806d686f4f3ce8a565068d78eabb835b
Geoffrey Hanson <g.hanson.b@gmail.com>
2018-12-08 12:24:46 -0600

    Fixed mongoose issue

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 // const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,15 +8,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
 
 // app.use(routes);
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/textadventure";
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/textadventure';
 mongoose.connect(MONGODB_URI || { useNewUrlParser: true });
 
-app.listen(PORT, function() {
+app.listen(PORT, () => {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
