f7a7cf48a18dbacaa2bd9f192302a3845dfdf9e2
Black Jack <hibbarcd@gmail.com>
2018-12-11 17:51:00 -0600

    front game page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navigation from './components/nav/navbar';
+import Card from './components/Card/card'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Card />
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card/card.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Jumbotron, Container } from 'reactstrap';
+
+const Card = (props) => {
+  return (
+    <div>
+      <Jumbotron fluid>
+        <Container fluid>
+          <h1 className="display-3">Main Workspace</h1>
+        </Container>
+      </Jumbotron>
+    </div>
+  );
+};
+
+export default Card;
