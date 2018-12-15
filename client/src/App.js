b6a9fb3dfee3f9ca08e761731480e4f35baf6593
Black Jack <hibbarcd@gmail.com>
2018-12-15 12:27:37 -0600

    database work in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import Navigation from './components/nav/navbar';
+import Navigation from './components/Nav/navbar';
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Card from './components/Card/Card';
 import './App.css';
 
 function App() {
   return (
+    // <Router>
     <div className="App">
       <Card />
+      {/* <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/books" component={Books} />
+          <Route exact path="/books/:id" component={Detail} />
+          <Route component={NoMatch} />
+        </Switch> */}
       <Navigation />
     </div>
+    /* </Router> */
   );
 }
 
