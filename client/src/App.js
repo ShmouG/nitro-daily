9a7fcda97a798f247f6029525c65599a84c92844
Geoffrey Hanson <g.hanson.b@gmail.com>
2018-12-15 11:27:29 -0600

    fixed error

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Nav from './components/Nav';
+// import Nav from './components/Nav';
 import Login from './pages/Login';
 import Interface from './pages/Interface';
 import NoMatch from './pages/NoMatch';
