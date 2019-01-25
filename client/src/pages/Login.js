a681bc9b2bcb90ac94dcb2302ad8e6274ac2452b
Geoffrey Hanson <g.hanson.b@gmail.com>
2019-01-25 10:44:38 +0100

    More commenting out

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,44 +1,44 @@
-import React from 'react';
-import { BrowserRouter as Route } from 'react-router-dom';
-import { Jumbotron, Container } from 'reactstrap';
-import LoginForm from '../components/LoginForm';
-import API from '../utils/API';
-import Interface from './Interface';
+// import React from 'react';
+// import { BrowserRouter as Route } from 'react-router-dom';
+// import { Jumbotron, Container } from 'reactstrap';
+// import LoginForm from '../components/LoginForm';
+// import API from '../utils/API';
+// import Interface from './Interface';
 
-const handleInputChange = (event) => {
-  const { name, value } = event.target;
-  this.setState({
-    [name]: value,
-  });
-};
+// const handleInputChange = (event) => {
+//   const { name, value } = event.target;
+//   this.setState({
+//     [name]: value,
+//   });
+// };
 
-const handleFormSubmit = (event) => {
-  event.preventDefault();
-  API.saveUser({
-    email: this.state.email,
-    pw: this.state.pw,
-  })
-    .then((res) => {
-      if (res.data.status === 'error') {
-        throw new Error(res.data.message);
-      }
-      this.setState({ results: res.data.message, error: '' });
-    })
-    .catch(err => this.setState({ error: err.message }));
-};
+// const handleFormSubmit = (event) => {
+//   event.preventDefault();
+//   API.saveUser({
+//     email: this.state.email,
+//     pw: this.state.pw,
+//   })
+//     .then((res) => {
+//       if (res.data.status === 'error') {
+//         throw new Error(res.data.message);
+//       }
+//       this.setState({ results: res.data.message, error: '' });
+//     })
+//     .catch(err => this.setState({ error: err.message }));
+// };
 
-const Login = () => (
-  <div>
-    <Jumbotron fluid>
-      <Container fluid>
-        <h3 className="display-3">Login</h3>
-        <LoginForm
-          handleInputChange={handleInputChange}
-          handleFormSubmit={<Route exact path="/" component={Interface} />}
-        />
-      </Container>
-    </Jumbotron>
-  </div>
-);
+// const Login = () => (
+//   <div>
+//     <Jumbotron fluid>
+//       <Container fluid>
+//         <h3 className="display-3">Login</h3>
+//         <LoginForm
+//           handleInputChange={handleInputChange}
+//           handleFormSubmit={<Route exact path="/" component={Interface} />}
+//         />
+//       </Container>
+//     </Jumbotron>
+//   </div>
+// );
 
-export default Login;
+// export default Login;
